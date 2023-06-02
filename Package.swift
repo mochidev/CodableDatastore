829eb441e787481d7561e1cf04a22790446c40f9
Dimitri Bouniol <dimitribouniol@mochidev.com>
2023-06-02 15:51:34 -0700

    Cleaned up default package files

--- Package.swift
+++ Package.swift
@@ -12,18 +12,16 @@ let package = Package(
         .watchOS(.v6),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CodableDatastore",
-            targets: ["CodableDatastore"]),
+            targets: ["CodableDatastore"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CodableDatastore"),
+        .target( name: "CodableDatastore"),
         .testTarget(
             name: "CodableDatastoreTests",
-            dependencies: ["CodableDatastore"]),
+            dependencies: ["CodableDatastore"]
+        ),
     ]
 )
