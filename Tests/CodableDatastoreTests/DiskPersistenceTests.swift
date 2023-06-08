8644898a78e3b4bc271e8ca2121344bb889824d6
Dimitri Bouniol <dimitribouniol@mochidev.com>
2023-06-07 21:22:15 -0700

    Added tests and implementation for creating a persistence store

--- Tests/CodableDatastoreTests/DiskPersistenceTests.swift
+++ Tests/CodableDatastoreTests/DiskPersistenceTests.swift
@@ -10,6 +10,16 @@ import XCTest
 @testable import CodableDatastore
 
 final class DiskPersistenceTests: XCTestCase {
+    var temporaryStoreURL: URL = FileManager.default.temporaryDirectory
+    
+    override func setUp() async throws {
+        temporaryStoreURL = FileManager.default.temporaryDirectory.appendingPathComponent(ProcessInfo.processInfo.globallyUniqueString, isDirectory: true);
+    }
+    
+    override func tearDown() async throws {
+        try? FileManager.default.removeItem(at: temporaryStoreURL)
+    }
+    
     func testTypes() throws {
 #if !os(Linux)
         XCTAssertTrue(type(of: try DiskPersistence.defaultStore() as Any) is DiskPersistence<ReadWrite>.Type)
@@ -43,4 +53,36 @@ final class DiskPersistenceTests: XCTestCase {
         XCTAssertEqual(url2.absoluteString, defaultDirectory)
     }
 #endif
+    
+    func testNoFileCreatedOnInit() async throws {
+        _ = try DiskPersistence(readWriteURL: temporaryStoreURL)
+        XCTAssertThrowsError(try temporaryStoreURL.checkResourceIsReachable())
+        
+        _ = DiskPersistence(readOnlyURL: temporaryStoreURL)
+        XCTAssertThrowsError(try temporaryStoreURL.checkResourceIsReachable())
+    }
+    
+    func testThrowsWithRemoteURLs() async throws {
+        XCTAssertThrowsError(try DiskPersistence(readWriteURL: URL(string: "https://apple.com/")!))
+    }
+    
+    func testStoreCreatedWhenAsked() async throws {
+        let persistence = try DiskPersistence(readWriteURL: temporaryStoreURL)
+        try await persistence.createPersistenceIfNecessary()
+        
+        XCTAssertTrue(try temporaryStoreURL.checkResourceIsReachable())
+        XCTAssertTrue(try temporaryStoreURL.appendingPathComponent("Snapshots", isDirectory: true).checkResourceIsReachable())
+        XCTAssertTrue(try temporaryStoreURL.appendingPathComponent("Backups", isDirectory: true).checkResourceIsReachable())
+    }
+    
+    func testStoreCreatesOnlyIfNecessary() async throws {
+        let persistence = try DiskPersistence(readWriteURL: temporaryStoreURL)
+        try await persistence.createPersistenceIfNecessary()
+        // This second time should be a no-op and shouldn't throw
+        try await persistence.createPersistenceIfNecessary()
+        
+        XCTAssertTrue(try temporaryStoreURL.checkResourceIsReachable())
+        XCTAssertTrue(try temporaryStoreURL.appendingPathComponent("Snapshots", isDirectory: true).checkResourceIsReachable())
+        XCTAssertTrue(try temporaryStoreURL.appendingPathComponent("Backups", isDirectory: true).checkResourceIsReachable())
+    }
 }
