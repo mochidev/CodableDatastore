fef3101c6f4e7ec40a9ea52209142b8a202ca71c
Dimitri Bouniol <dimitribouniol@mochidev.com>
2023-06-18 01:36:28 -0700

    Added version data to cursor lookups, and renamed Cursor to CursorProtocol

--- Sources/CodableDatastore/Persistence/Cursor.swift
+++ Sources/CodableDatastore/Persistence/Cursor.swift
@@ -9,15 +9,15 @@
 /// An opaque type ``Persistence``s may use to indicate a position in their storage.
 ///
 /// - Note: A cursor is only valid within the same transaction for the same persistence it was created for.
-public protocol Cursor<P> {
-    associatedtype P: Persistence
+public protocol CursorProtocol<P> {
+    associatedtype P: _Persistence
     var persistence: P { get }
     
 //    var transaction: Transaction<P> { get }
 }
 
 /// An opaque type ``Persistence``s may use to indicate the position of an instance in their storage.
-public protocol InstanceCursor: Cursor {}
+public protocol InstanceCursorProtocol<P>: InsertionCursorProtocol {}
 
 /// An opaque type ``Persistence``s may use to indicate the position a new instance should be inserted in their storage.
-public protocol InsertionCursor: Cursor {}
+public protocol InsertionCursorProtocol<P>: CursorProtocol {}
