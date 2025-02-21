9c8c7bd005f5a9fa6c14a0a83070a81776a1ddb2
Dimitri Bouniol <dimitribouniol@mochidev.com>
2025-02-21 03:12:12 -0800

    Updated syntax formatting in error types

--- Sources/CodableDatastore/Persistence/DatastoreInterfaceError.swift
+++ Sources/CodableDatastore/Persistence/DatastoreInterfaceError.swift
@@ -46,27 +46,27 @@ public enum DatastoreInterfaceError: LocalizedError {
     public var errorDescription: String? {
         switch self {
         case .multipleRegistrations:
-            return "The datastore has already been registered with another persistence. Make sure to only register a datastore with a single persistence."
+            "The datastore has already been registered with another persistence. Make sure to only register a datastore with a single persistence."
         case .alreadyRegistered:
-            return "The datastore has already been registered with this persistence. Make sure to not call register multiple times per persistence."
+            "The datastore has already been registered with this persistence. Make sure to not call register multiple times per persistence."
         case .datastoreNotFound:
-            return "The datastore was not found and has likely not been registered with this persistence."
+            "The datastore was not found and has likely not been registered with this persistence."
         case .duplicateWriters:
-            return "An existing datastore that can write to the persistence has already been registered for this key. Only one writer is suppored per key."
+            "An existing datastore that can write to the persistence has already been registered for this key. Only one writer is suppored per key."
         case .instanceNotFound:
-            return "The requested instance could not be found with the specified identifier."
+            "The requested instance could not be found with the specified identifier."
         case .instanceAlreadyExists:
-            return "The requested insertion cursor conflicts with an already existing identifier."
+            "The requested insertion cursor conflicts with an already existing identifier."
         case .datastoreKeyNotFound:
-            return "The datastore being manipulated does not yet exist in the persistence."
+            "The datastore being manipulated does not yet exist in the persistence."
         case .indexNotFound:
-            return "The index being manipulated does not yet exist in the datastore."
+            "The index being manipulated does not yet exist in the datastore."
         case .transactionInactive:
-            return "The transaction was accessed outside of its activity window. Please make sure the transaction wasn't escaped."
+            "The transaction was accessed outside of its activity window. Please make sure the transaction wasn't escaped."
         case .unknownCursor:
-            return "The cursor does not match the one provided by the persistence."
+            "The cursor does not match the one provided by the persistence."
         case .staleCursor:
-            return "The cursor no longer refers to fresh data. Please make sure to use them as soon as possible and not interspaced with other writes."
+            "The cursor no longer refers to fresh data. Please make sure to use them as soon as possible and not interspaced with other writes."
         }
     }
 }
