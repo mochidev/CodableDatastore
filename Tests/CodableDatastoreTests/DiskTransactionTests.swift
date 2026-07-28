4565acd0d966205bf746abce510cb72fbce4bd68
Dimitri Bouniol <dimitribouniol@mochidev.com>
2026-07-28 05:04:20 -0700

    Added test showcasing that transactions could start at the same time

--- Tests/CodableDatastoreTests/DiskTransactionTests.swift
+++ Tests/CodableDatastoreTests/DiskTransactionTests.swift
@@ -70,4 +70,162 @@ final class DiskTransactionTests: XCTestCase, @unchecked Sendable {
             XCTAssertEqual(existingDescriptor, descriptor)
         }
     }
+    
+    func testSerialMutableRootTransactions() async throws {
+        let persistence = try DiskPersistence(readWriteURL: temporaryStoreURL)
+        
+        struct TestFormat: DatastoreFormat {
+            enum Version: Int, CaseIterable {
+                case zero
+            }
+            
+            struct Instance: Codable {}
+            typealias Identifier = UUID
+            
+            static let defaultKey: DatastoreKey = "test"
+            static let currentVersion = Version.zero
+        }
+        
+        var tasks: [Task<Void, Never>] = []
+        
+        let iterations = 5000
+        nonisolated(unsafe) var count = 0
+        nonisolated(unsafe) var lastStartedIndex = 0
+        for index in 0..<iterations {
+            tasks.append(Task {
+                let result = try? await persistence._withTransaction(actionName: nil, options: []) { _, _ in
+                    lastStartedIndex = index
+                    try? await Task.sleep(for: .seconds(Double.random(in: 0.0001...0.001)))
+                    count += 1
+                    XCTAssertEqual(lastStartedIndex, index)
+                    return index
+                }
+                
+                XCTAssertEqual(result, index)
+            })
+        }
+        
+        for task in tasks {
+            await task.value
+        }
+        XCTAssertEqual(count, iterations)
+    }
+    
+    func testConcurrentReadOnlyRootTransactions() async throws {
+        let persistence = try DiskPersistence(readWriteURL: temporaryStoreURL)
+        
+        struct TestFormat: DatastoreFormat {
+            enum Version: Int, CaseIterable {
+                case zero
+            }
+            
+            struct Instance: Codable {}
+            typealias Identifier = UUID
+            
+            static let defaultKey: DatastoreKey = "test"
+            static let currentVersion = Version.zero
+        }
+        
+        var tasks: [Task<Void, Never>] = []
+        
+        let iterations = 5000
+        nonisolated(unsafe) var count = 0
+        for index in 0..<iterations {
+            tasks.append(Task {
+                let result = try? await persistence._withTransaction(actionName: nil, options: [.readOnly]) { _, _ in
+                    try? await Task.sleep(for: .seconds(Double.random(in: 0.0001...0.001)))
+                    count += 1
+                    return index
+                }
+                
+                XCTAssertEqual(result, index)
+            })
+        }
+        
+        for task in tasks {
+            await task.value
+        }
+        XCTAssertLessThanOrEqual(count, iterations)
+    }
+    
+    func testSerialMutableChildTransactions() async throws {
+        let persistence = try DiskPersistence(readWriteURL: temporaryStoreURL)
+        
+        struct TestFormat: DatastoreFormat {
+            enum Version: Int, CaseIterable {
+                case zero
+            }
+            
+            struct Instance: Codable {}
+            typealias Identifier = UUID
+            
+            static let defaultKey: DatastoreKey = "test"
+            static let currentVersion = Version.zero
+        }
+        
+        nonisolated(unsafe) var tasks: [Task<Void, Never>] = []
+        
+        let iterations = 5000
+        nonisolated(unsafe) var count = 0
+        nonisolated(unsafe) var lastStartedIndex = 0
+        try await persistence._withTransaction(actionName: nil, options: []) { _, _ in
+            for index in 0..<iterations {
+                tasks.append(Task {
+                    let result = try? await persistence._withTransaction(actionName: nil, options: []) { _, _ in
+                        lastStartedIndex = index
+                        try? await Task.sleep(for: .seconds(Double.random(in: 0.0001...0.001)))
+                        count += 1
+                        XCTAssertEqual(lastStartedIndex, index)
+                        return index
+                    }
+                    
+                    XCTAssertEqual(result, index)
+                })
+            }
+            
+            for task in tasks {
+                await task.value
+            }
+        }
+        XCTAssertEqual(count, iterations)
+    }
+    
+    func testConcurrentReadOnlyChildTransactions() async throws {
+        let persistence = try DiskPersistence(readWriteURL: temporaryStoreURL)
+        
+        struct TestFormat: DatastoreFormat {
+            enum Version: Int, CaseIterable {
+                case zero
+            }
+            
+            struct Instance: Codable {}
+            typealias Identifier = UUID
+            
+            static let defaultKey: DatastoreKey = "test"
+            static let currentVersion = Version.zero
+        }
+        
+        nonisolated(unsafe) var tasks: [Task<Void, Never>] = []
+        
+        let iterations = 5000
+        nonisolated(unsafe) var count = 0
+        try await persistence._withTransaction(actionName: nil, options: []) { _, _ in
+            for index in 0..<iterations {
+                tasks.append(Task {
+                    let result = try? await persistence._withTransaction(actionName: nil, options: [.readOnly]) { _, _ in
+                        try? await Task.sleep(for: .seconds(Double.random(in: 0.0001...0.001)))
+                        count += 1
+                        return index
+                    }
+                    
+                    XCTAssertEqual(result, index)
+                })
+            }
+            
+            for task in tasks {
+                await task.value
+            }
+        }
+        XCTAssertLessThanOrEqual(count, iterations)
+    }
 }
